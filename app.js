697bb54b6d1deae540ebfc13f4c39d6a77643219
Sam Roberts <sam.roberts@venturonet.com>
2023-09-26 14:15:42 +0100

    DWA

--- app.js
+++ app.js
@@ -10,29 +10,34 @@ const webUI = require("./webUI");
 
 app.set("view engine", "ejs");
 
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS ping_results (
-      id INTEGER PRIMARY KEY,
-      host TEXT,
-      timestamp TIMESTAMP,
-      response_time INTEGER,
-      packet_loss REAL
-    )
-  `);
-});
+// Move database setup code to a separate function
+function setupDatabase() {
+  const db = new sqlite3.Database("ping_results.db");
+  db.serialize(() => {
+    db.run(`
+      CREATE TABLE IF NOT EXISTS ping_results (
+        id INTEGER PRIMARY KEY,
+        host TEXT,
+        timestamp TIMESTAMP,
+        response_time INTEGER,
+        packet_loss REAL
+      )
+    `);
+  });
 
-db.close((err) => {
-  if (err) {
-    console.error("Error closing the database connection:", err);
-  } else {
-    console.log("Database is ready.");
-  }
-});
-// Initialize ping monitoring
+  db.close((err) => {
+    if (err) {
+      console.error("Error closing the database connection:", err);
+    } else {
+      console.log("Database is ready.");
+    }
+  });
+}
+
+setupDatabase();
+
 pingMonitor.start();
 
-// Serve the web UI
 app.use(express.static("public"));
 app.use("/ping-results", webUI);
 
