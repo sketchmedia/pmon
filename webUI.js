2c4580fdbb3eb72812ea33ee56e6ecc57587f4c5
Sam Roberts <sam.roberts@venturonet.com>
2023-09-26 13:59:55 +0100

    filters

--- webUI.js
+++ webUI.js
@@ -6,25 +6,28 @@ router.get("/", (req, res) => {
   res.render("chart");
 });
 
-// Modify the route to return JSON data
+// Modify the route to return JSON data within the specified date range
 router.get("/data", (req, res) => {
+  const startDate = req.query.startDate; // Convert start date to Unix timestamp
+  const endDate = req.query.endDate; // Convert end date to Unix timestamp
+
   const db = new sqlite3.Database("ping_results.db");
-  db.all(
-    "SELECT timestamp, response_time, packet_loss FROM ping_results",
-    (err, rows) => {
-      if (err) {
-        console.error("Error retrieving data from the database:", err);
-        res.status(500).json({error: "Internal Server Error"});
-        return;
-      }
+  const query =
+    "SELECT timestamp, response_time, packet_loss FROM ping_results WHERE timestamp >= ? AND timestamp <= ?";
 
-      const timestamps = rows.map((row) => row.timestamp);
-      const responseTimes = rows.map((row) => row.response_time);
-      const packetLoss = rows.map((row) => row.packet_loss);
-
-      res.json({timestamps, responseTimes, packetLoss});
+  db.all(query, [startDate, endDate], (err, rows) => {
+    if (err) {
+      console.error("Error retrieving data from the database:", err);
+      res.status(500).json({error: "Internal Server Error"});
+      return;
     }
-  );
+
+    const timestamps = rows.map((row) => new Date(row.timestamp).toISOString()); // Convert Unix timestamp to ISO format
+    const responseTimes = rows.map((row) => row.response_time);
+    const packetLoss = rows.map((row) => row.packet_loss);
+
+    res.json({timestamps, responseTimes, packetLoss});
+  });
   db.close();
 });
 
