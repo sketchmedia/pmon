0f52c958a367b3d82d1f5bb4943908a41d95dd3c
Sam Roberts <sam.roberts@venturonet.com>
2023-09-27 17:07:22 +0100

    moving average and standard deviation for response times

--- pingMonitor.js
+++ pingMonitor.js
@@ -1,12 +1,10 @@
 const ping = require("ping");
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("ping_results.db");
+
 const hostToPing = "google.co.uk";
 
 const start = () => {
   const schedule = require("node-schedule");
-
-  const thresholdFactor = 1.5; // Adjust this factor as needed to control sensitivity.
 
   schedule.scheduleJob("*/5 * * * * *", async () => {
     try {
@@ -15,37 +13,17 @@ const start = () => {
         const {time} = res;
         const packetLoss = res.packetLoss || 0;
 
-        // Calculate the average ping time from historical data in the database.
-        db.all("SELECT response_time FROM ping_results", [], (err, rows) => {
-          if (err) {
-            console.error(
-              "Error reading historical data from the database:",
-              err
-            );
-            return;
+        const db = new sqlite3.Database("ping_results.db");
+        db.run(
+          `INSERT INTO ping_results (host, timestamp, response_time, packet_loss) VALUES (?, ?, ?, ?)`,
+          [hostToPing, new Date(), time, packetLoss],
+          (err) => {
+            if (err) {
+              console.error("Error inserting data into the database:", err);
+            }
           }
-
-          const historicalPingTimes = rows.map((row) => row.response_time);
-          const averagePingTime =
-            historicalPingTimes.reduce((acc, val) => acc + val, 0) /
-            historicalPingTimes.length;
-
-          // Define a threshold as a factor of the average ping time.
-          const threshold = averagePingTime * thresholdFactor;
-
-          if (Math.abs(time - averagePingTime) >= threshold) {
-            // Insert the new ping result into the database.
-            db.run(
-              `INSERT INTO ping_results (host, timestamp, response_time, packet_loss) VALUES (?, ?, ?, ?)`,
-              [hostToPing, new Date(), time, packetLoss],
-              (err) => {
-                if (err) {
-                  console.error("Error inserting data into the database:", err);
-                }
-              }
-            );
-          }
-        });
+        );
+        db.close();
       } else {
         console.error(`Host ${hostToPing} is down.`);
       }
@@ -53,32 +31,6 @@ const start = () => {
       console.error("Error pinging the host:", error);
     }
   });
-
-  //   schedule.scheduleJob("*/5 * * * * *", async () => {
-  //     try {
-  //       const res = await ping.promise.probe(hostToPing);
-  //       if (res.alive) {
-  //         const {time} = res;
-  //         const packetLoss = res.packetLoss || 0;
-
-  //         const db = new sqlite3.Database("ping_results.db");
-  //         db.run(
-  //           `INSERT INTO ping_results (host, timestamp, response_time, packet_loss) VALUES (?, ?, ?, ?)`,
-  //           [hostToPing, new Date(), time, packetLoss],
-  //           (err) => {
-  //             if (err) {
-  //               console.error("Error inserting data into the database:", err);
-  //             }
-  //           }
-  //         );
-  //         db.close();
-  //       } else {
-  //         console.error(`Host ${hostToPing} is down.`);
-  //       }
-  //     } catch (error) {
-  //       console.error("Error pinging the host:", error);
-  //     }
-  //   });
 };
 
 module.exports = {start};
--- webUI.js
+++ webUI.js
@@ -7,13 +7,48 @@ router.get("/", (req, res) => {
 });
 
 // Modify the route to return JSON data within the specified date range
+// router.get("/data", (req, res) => {
+//   const startDate = req.query.startDate; // Convert start date to Unix timestamp
+//   const endDate = req.query.endDate; // Convert end date to Unix timestamp
+
+//   const db = new sqlite3.Database("ping_results.db");
+//   const query =
+//     "SELECT timestamp, response_time, packet_loss FROM ping_results WHERE timestamp >= ? AND timestamp <= ?";
+
+//   db.all(query, [startDate, endDate], (err, rows) => {
+//     if (err) {
+//       console.error("Error retrieving data from the database:", err);
+//       res.status(500).json({error: "Internal Server Error"});
+//       return;
+//     }
+
+//     const timestamps = rows.map((row) => new Date(row.timestamp).toISOString()); // Convert Unix timestamp to ISO format
+//     const responseTimes = rows.map((row) => row.response_time);
+//     const packetLoss = rows.map((row) => row.packet_loss);
+
+//     res.json({timestamps, responseTimes, packetLoss});
+//   });
+//   db.close();
+// });
 router.get("/data", (req, res) => {
   const startDate = req.query.startDate; // Convert start date to Unix timestamp
   const endDate = req.query.endDate; // Convert end date to Unix timestamp
 
   const db = new sqlite3.Database("ping_results.db");
-  const query =
-    "SELECT timestamp, response_time, packet_loss FROM ping_results WHERE timestamp >= ? AND timestamp <= ?";
+
+  // Define the window size for calculating the moving average and standard deviation.
+  const windowSize = 5; // Adjust this value as needed.
+
+  // Use SQL queries to retrieve data within the date range.
+  const query = `
+    SELECT
+      timestamp,
+      response_time,
+      packet_loss
+    FROM ping_results
+    WHERE timestamp >= ? AND timestamp <= ?
+    ORDER BY timestamp ASC
+  `;
 
   db.all(query, [startDate, endDate], (err, rows) => {
     if (err) {
@@ -22,13 +57,53 @@ router.get("/data", (req, res) => {
       return;
     }
 
-    const timestamps = rows.map((row) => new Date(row.timestamp).toISOString()); // Convert Unix timestamp to ISO format
-    const responseTimes = rows.map((row) => row.response_time);
-    const packetLoss = rows.map((row) => row.packet_loss);
+    // Calculate moving average and standard deviation for response times.
+    const filteredRows = [];
+    let responseTimeSum = 0;
+    let responseTimeSquaredSum = 0;
 
-    res.json({timestamps, responseTimes, packetLoss});
+    for (let i = 0; i < rows.length; i++) {
+      const responseTime = rows[i].response_time;
+
+      // Update sums for calculating average and standard deviation.
+      responseTimeSum += responseTime;
+      responseTimeSquaredSum += responseTime * responseTime;
+
+      // Add the row to the filtered list.
+      filteredRows.push(rows[i]);
+
+      // Check if we have enough data points for the window.
+      if (i >= windowSize - 1) {
+        // Calculate the moving average and standard deviation.
+        const average = responseTimeSum / windowSize;
+        const variance =
+          responseTimeSquaredSum / windowSize - average * average;
+        const standardDeviation = Math.sqrt(variance);
+
+        // Check if the current data point deviates significantly from the average.
+        if (Math.abs(responseTime - average) >= 2 * standardDeviation) {
+          // Highlight or store the data point as significant.
+          // You can add a flag or property to the row to indicate significance.
+          filteredRows[i].significant = true;
+        }
+
+        // Remove the oldest data point from the sums.
+        const removedResponseTime = rows[i - windowSize + 1].response_time;
+        responseTimeSum -= removedResponseTime;
+        responseTimeSquaredSum -= removedResponseTime * removedResponseTime;
+      }
+    }
+
+    const timestamps = filteredRows.map((row) =>
+      new Date(row.timestamp).toISOString()
+    );
+    const responseTimes = filteredRows.map((row) => row.response_time);
+    const packetLoss = filteredRows.map((row) => row.packet_loss);
+    const isSignificant = filteredRows.map((row) => row.significant || false);
+
+    res.json({timestamps, responseTimes, packetLoss, isSignificant});
   });
+
   db.close();
 });
-
 module.exports = router;
